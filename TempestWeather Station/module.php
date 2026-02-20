2f5db7672218fe0a0561fd9569d4143cdb705269
fisart <1234fisart@gmail.com>
2026-02-20 17:42:22 -0500

    fix: adjust yOffset for windbarb in chart data string to correct visual alignment

--- TempestWeather Station/module.php
+++ TempestWeather Station/module.php
@@ -519,7 +519,7 @@ class TempestWeatherStation extends IPSModule
                                         $barbPoints[] = "[$ts, $sVal, $dVal]";
                                     }
                                 }
-                                $dataString = "{ type: 'area', data: [" . implode(',', $speedPoints) . "], color: '$chartColor', fillOpacity: 0.3, zIndex: 1 }, { type: 'windbarb', data: [" . implode(',', $barbPoints) . "], color: '$fontColor', zIndex: 2, vectorLength: 12, yOffset: -30 }";
+                                $dataString = "{ type: 'area', data: [" . implode(',', $speedPoints) . "], color: '$chartColor', fillOpacity: 0.3, zIndex: 1 }, { type: 'windbarb', data: [" . implode(',', $barbPoints) . "], color: '$fontColor', zIndex: 2, vectorLength: 12, yOffset: 30 }";
                             }
                         } else {
                             foreach (array_reverse($history) as $row) {
